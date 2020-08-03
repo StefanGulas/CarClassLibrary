1f74cf1d736a3cfb41dd49d3642a1f0c2aef3899
Stefan <stefan.gulas@gmail.com>
2020-08-03 15:59:39 +0200

    Unit Test added

---
+++ CarShopConsoleTest/ClassTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using CarClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarShopConsoleTest
+{
+    [TestClass]
+    public class ClassTests
+    {
+        [TestMethod]
+        public void CarClassTest()
+        {
+            //Arrange
+            Car car = new Car();
+            string Make = "BMW";
+            string Model = "3er";
+            decimal Price = 34500M;
+
+            //Act
+            car.Make = "BMW";
+            car.Model = "3er";
+            car.Price = 34500M;
+
+            //Assert
+            Assert.AreEqual(Make, car.Make);
+            Assert.AreEqual(Model, car.Model);
+            Assert.AreEqual(Price, car.Price);
+        }
+        [TestMethod]
+        public void StoreClassTest()
+        {
+            //Arrange
+            Store store = new Store();
+
+            Car car = new Car();
+            string Make = "BMW";
+            string Model = "3er";
+            decimal Price = 34500M;
+
+            //Act
+            car.Make = "BMW";
+            car.Model = "3er";
+            car.Price = 34500M;
+            store.CarList.Add(car);
+
+            //Assert
+            Assert.AreEqual(Make, store.CarList[0].Make);
+            Assert.AreEqual(Model, store.CarList[0].Model);
+            Assert.AreEqual(Price, store.CarList[0].Price);
+        }
+
+        [TestMethod]
+        public void ClassTest()
+        {
+            //Arrange
+            Store store = new Store();
+            Car car1 = new Car();
+            Car car2 = new Car();
+            string Make = "BMW";
+            string Model = "3er";
+            decimal Price = 34500M;
+
+            string Make2 = "VW";
+            string Model2 = "Golf";
+            decimal Price2 = 22400M;
+            //Act
+            car1.Make = "BMW";
+            car1.Model = "3er";
+            car1.Price = 34500M;
+            car2.Make = "VW";
+            car2.Model = "Golf";
+            car2.Price = 22400M;
+            store.CarList.Add(car1);
+            store.CarList.Add(car2);
+
+            //Assert
+            Assert.AreEqual(Make, store.CarList[0].Make);
+            Assert.AreEqual(Model2, store.CarList[1].Model);
+            Assert.AreEqual(Price2, store.CarList[1].Price);
+            Assert.AreEqual(Price, store.CarList[0].Price);
+        }
+        [TestMethod]
+        public void MethodTest()
+        {
+            //Arrange
+            Store store = new Store();
+            Car car1 = new Car();
+            Car car2 = new Car();
+            string Make = "BMW";
+            string Model = "3er";
+            decimal Price = 34500M;
+
+            string Make2 = "VW";
+            string Model2 = "Golf";
+            decimal Price2 = 22400M;
+            car1.Make = "BMW";
+            car1.Model = "3er";
+            car1.Price = 34500M;
+            car2.Make = "VW";
+            car2.Model = "Golf";
+            car2.Price = 22400M;
+
+            decimal expectedTotal = car1.Price + (2 * car2.Price);
+            //Act
+            store.CarList.Add(car1);
+            store.CarList.Add(car2);
+
+            store.ShoppingList.Add(car1);
+            store.ShoppingList.Add(car2);
+            store.ShoppingList.Add(car2);
+
+            var actualTotal = store.Checkout();
+            
+
+            //Assert
+            Assert.AreEqual(expectedTotal, actualTotal);
+        }
+
+    }
+}
---
+++ CarShopConsoleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CarShopConsoleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CarShopConsoleTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("caa4f138-68a3-496e-9d4d-d8b2ec9a03c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
